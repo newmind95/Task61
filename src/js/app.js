8d249481bb38d3554e81e516c5a049a9133bcee2
Kristiyan <javakris95@gmx.com>
2022-12-07 13:54:05 +0200

    add fire img

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,12 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  
+  // There must be a fire emoji next to the price when it has the .hot class
+  var elements = document.querySelectorAll('.price')
+  elements.forEach((element) => {
+    if (element.classList.contains('price')) {
+      element.innerHTML += "&#128293"
+    }
+  })
 });

955a922c0b4eab792aa0a8aeb765f96c27da74fb
Kristiyan <javakris95@gmx.com>
2022-12-07 13:56:59 +0200

    add fire img

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,10 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  var elements = document.querySelectorAll('.price')
+  elements.forEach((element) => {
+    if (element.classList.contains('hot')) {
+      element.innerHTML += "&#128293"
+    }
+  })
 });
